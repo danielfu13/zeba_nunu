09e356a8152461983b9251c0f07f448cc25dee19
danielfu13 <daniel.fu123@hotmail.com>
2022-07-11 19:52:18 -0400

    created routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,34 @@
 const router = require('express').Router();
+const fs = require('fs');
+const db = require('../Develop/db/db.json')
 
+router.get('/api/notes', (req, res) => {
+    fs.readFile(db, (err, data) => {
+        if (err) throw err;
+        var note = JSON.parse(data);
+        res.json(note);
+    });
+});
 
+router.post('/api/notes', (req, res) => {
+    fs.readFile(db, (err, data) => {
+        if (err) throw err;
 
+        var note = JSON.parse(data);
+        const noteRequest = req.body;
+        const newNote = {
+            title: noteRequest.title,
+            text: noteRequest.text
+        };
+
+        note.push(newNote);
+        res.json(newNote);
+        fs.writeFile(db, JSON.stringify(note), (err) => {
+            if (err) throw err;
+
+            res.send('successfully added');
+        })
+    });
+});
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,19 @@
+// const path = require('path');
+// const router = require('express').Router();
+
+// module.exports = router;
+
+
+
 const path = require('path');
 const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
+})
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
+});
+
 
 module.exports = router;
